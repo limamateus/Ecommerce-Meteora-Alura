e8363abc809877210201a7cfeb00394f296ada8c
Mateus Rodrigues De Lima <70700338+limamateus@users.noreply.github.com>
2024-03-21 19:59:15 -0300

    Primeiro Commit

---
+++ src/context/CarrinhoContext.jsx
@@ -0,0 +1,15 @@
+import { createContext, useState } from "react";
+
+
+export const CarrinhoContext = createContext();
+
+
+
+export const CarrinhoProvider = ({children}) =>{
+    const [carrinho , setCarrinho] = useState([]);
+    return(
+        <CarrinhoContext.Provider value={{carrinho, setCarrinho}}>
+            {children}
+        </CarrinhoContext.Provider>
+    )
+}
